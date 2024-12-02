71802843fcb66ea5f32dc58a418702670bb72063
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-02 10:42:59 +0200

    Back-end - fix migrat create users

--- laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $password->string("password");
+            $table->string("password");
             $table->timestamps();
             $table->string('name');
         });
