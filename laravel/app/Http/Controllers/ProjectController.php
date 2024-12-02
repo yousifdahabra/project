a7a767e666ece1e10e6f4cdf98a4258329801c85
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-02 11:56:56 +0200

    Back-end - update user and project Controller

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -3,8 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function add_project(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $project = Project::create([
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'project add successfully',
+            'project' => $project
+        ],201);
+    }
+
+
+    public function update_project(Request $request,$id){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails() && $id != '') {
+            return response()->json($validator->errors(), 400);
+        }
+        $description = Project::findOrFail($id);
+
+        $description::update([
+            'name' => $request->name,
+            'description' => $request->email,
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Project Update successfully'
+        ],201);
+    }
+
+    public function delete_project(Request $request,$id){
+
+        if(empty($id)){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'ID not found'
+            ],401);
+        }
+
+        $project = Project::findOrFail($id);
+        $project->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Project deleted successfully'
+        ],201);
+
+    }
+
 }
--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -22,7 +22,47 @@ class UserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-        return response()->json(compact('user'), 201);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User add successfully',
+            'user' => $user
+        ],201);
+    }
+
+
+    public function update_user(Request $request,$id){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails() && $id != '') {
+            return response()->json($validator->errors(), 400);
+        }
+        $user = User::findOrFail($id);
+
+        $user::update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User Update successfully'
+        ],201);
+    }
+
+    public function delete_user(Request $request,$id){
+
+        $user = User::findOrFail($id);
+        $user->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User deleted successfully'
+        ],201);
+
     }
 
 }
