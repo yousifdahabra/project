29127f1c8c92e9437bb7bdb5a0874e12b36e5b95
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-02 11:08:01 +0200

    Back-end - update project model fillable

--- laravel/app/Models/Project.php
+++ laravel/app/Models/Project.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'memeber_id',
+    ];
+
 }
