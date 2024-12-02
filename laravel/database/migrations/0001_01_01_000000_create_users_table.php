3927eaf8fec7506cc92e00f6eede507dc4240244
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-12-02 10:45:06 +0200

    Back-end - fix migrate user table

--- laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string("password");
             $table->timestamps();
-            $table->string('name');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
